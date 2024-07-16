a13ad3b6e2c06188b5564d59cf2648f342be664b
henrique <henriquebiehl@gmail.com>
2024-07-15 21:39:06 -0300

    Agora funciona, sem leaks, para o caso sem bound e com calculo de viabilide

--- Trab2/main.c
+++ Trab2/main.c
@@ -68,10 +68,34 @@ struct listaCandidatos_t *destroiLista(struct listaCandidatos_t *l){
     return NULL;
 }
 
+void insereElementoFimLista(struct listaCandidatos_t *l, struct nodoLista_t *n){
+    struct nodoLista_t *aux; 
+
+    n->prox = NULL; 
+    aux = l->fim; 
+    
+    if(l->fim == NULL){
+        l->inicio = n; 
+        n->ant = NULL; 
+    }
+    else{
+        aux->prox = n; 
+        n->ant = aux; 
+    }
+
+    l->fim = n; 
+    l->tamanho++;
+}
 
 int insereElementoLista(struct listaCandidatos_t *l, struct nodoLista_t *n){
     struct nodoLista_t *aux, *anterior;
     unsigned int acheiMaximo = 0;
+
+    if(n == NULL)
+        return 0;
+
+    n->ant = NULL;
+    n->prox = NULL;
 
     if(l->inicio == NULL){
         l->inicio = n; 
@@ -81,7 +105,7 @@ int insereElementoLista(struct listaCandidatos_t *l, struct nodoLista_t *n){
     }
 
     aux = l->inicio; 
-    anterior = aux;
+    anterior = NULL;
 
     while(aux != NULL && !acheiMaximo){
         if(aux->id <= n->id)
@@ -95,8 +119,9 @@ int insereElementoLista(struct listaCandidatos_t *l, struct nodoLista_t *n){
     n->prox = anterior->prox; 
     anterior->prox = n;
 
-    if(anterior->prox == NULL)        
+    if(n->prox == NULL){        
         l->fim = n; 
+    }
     else 
         anterior->prox->ant = n ;
 
@@ -105,14 +130,35 @@ int insereElementoLista(struct listaCandidatos_t *l, struct nodoLista_t *n){
     return 1;
 }
 
+
+struct nodoLista_t *removeUltimoElementoLista(struct listaCandidatos_t *l){
+    struct nodoLista_t *n; 
+
+    n = l->fim; 
+
+    l->fim = n->ant;
+    if(n == l->inicio)
+        l->inicio = NULL; 
+    else{
+        n->ant->prox = NULL;
+    }
+
+    l->tamanho--;
+
+    return n;
+}
+
 struct nodoLista_t *removePrimeiroElemento(struct listaCandidatos_t *l){
     struct nodoLista_t *n; 
 
+    if(l->inicio == NULL)
+        return NULL; 
+
     n = l->inicio; 
 
-    l->inicio = l->inicio->prox; 
+    l->inicio = n->prox; 
     
-    if(n == l->fim)
+    if(l->inicio == NULL)
         l->fim = NULL;
     else
         l->inicio->ant = NULL;
@@ -135,7 +181,6 @@ struct nodoLista_t *copiaNodo(struct nodoLista_t *n){
     maxSize = n->grupo[0] + 1;
     g = malloc(maxSize*sizeof(unsigned int));
 
-    printf("maxSize %d e numGrupo %d\n", maxSize, n->grupo[0]);
     for(int i=0; i < maxSize; ++i)
         g[i] = n->grupo[i]; 
     c->grupo = g;
@@ -173,7 +218,6 @@ int temTodosOsGrupos(struct listaCandidatos_t *l, unsigned int totalGrupos){
         while(n != NULL && !possuiGrupo){
             for(int i = 1; i < n->grupo[0]+1 && !possuiGrupo; ++i){
                 if(n->grupo[i] == g){
-                    printf("    %d tem %d\n", n->id, g);
                     possuiGrupo = 1;
                 }
             }
@@ -181,18 +225,31 @@ int temTodosOsGrupos(struct listaCandidatos_t *l, unsigned int totalGrupos){
         }
 
         if(!possuiGrupo){
-            printf("grupo %d\n",  g);
             return 0; 
         }
         
     }
     
-    printf("Deu boa\n");
     return 1;
 }
 
-void candidatos_branch_and_bound(struct listaCandidatos_t *C, struct listaCandidatos_t *S, unsigned int n, unsigned int l){
+void printLista(struct listaCandidatos_t *l){
+    struct nodoLista_t *n; 
 
+    n = l->inicio;
+    while(n != NULL){
+        printf("id: %d [ ", n->id);
+        for(int i=1; i < n->grupo[0]+1; ++i)
+            printf("%d ",n->grupo[i]);
+        printf("]\n");
+        n = n->prox;
+    }
+    printf("\n");
+
+}
+
+void candidatos_branch_and_bound(struct listaCandidatos_t *C, struct listaCandidatos_t *S, unsigned int n, unsigned int l){
+        
     if(temTodosOsGrupos(S,n)){
         if(S->tamanho < melhorLista->tamanho){
             struct listaCandidatos_t *aux; 
@@ -201,21 +258,63 @@ void candidatos_branch_and_bound(struct listaCandidatos_t *C, struct listaCandid
             destroiLista(aux);
         }
     }
+
+    struct nodoLista_t *x; 
+    struct listaCandidatos_t *c; 
+
+    c = copiaLista(C); 
+    x = removePrimeiroElemento(c);
+    while(x != NULL){
+        insereElementoLista(S, x); 
+        candidatos_branch_and_bound(c, S, n, l+1);    
+        removeUltimoElementoLista(S);    
+        destroiNodo(x);
+        x = removePrimeiroElemento(c);
+    } 
+    destroiLista(c);
 }
 
-void printLista(struct listaCandidatos_t *l){
-    struct nodoLista_t *n; 
+/*
+void candidatos_branch_and_bound(struct listaCandidatos_t *C, struct listaCandidatos_t *S, unsigned int n, unsigned int l){
+    
+    printf("SELECIONADOS: \n");
+    printLista(S);
+    printf("CANDIDATOS: \n");
+    printLista(C);
 
-    n = l->inicio;
-    while(n != NULL){
-        printf("id: %d\n", n->id);
-        for(int i=1; i < n->grupo[0]+1; ++i)
-            printf("    grupo %d\n",n->grupo[i]);
-        n = n->prox;
+    
+    if(temTodosOsGrupos(S,n)){
+        if(S->tamanho < melhorLista->tamanho){
+            struct listaCandidatos_t *aux; 
+            aux = melhorLista;
+            melhorLista = copiaLista(S);
+            destroiLista(aux);
+        }
     }
-    printf("\n");
 
+    struct nodoLista_t *x, *aux; 
+
+    x = removePrimeiroElemento(C);
+    aux = x;  
+    while(x != NULL){
+        insereElementoLista(S, x); 
+        candidatos_branch_and_bound(C, S, n, l+1);    
+        aux = removeUltimoElementoLista(S);    
+        if(l == 0)
+            destroiNodo(x);
+        x = removePrimeiroElemento(C);  
+
+
+    } 
+
+    if(l > 0 && aux != NULL){
+        printf("    inserindo %d na lista de novo\n", aux->id);
+        aux = copiaNodo(aux);
+        insereElementoLista(C, aux);
+        printLista(C);
+    }
 }
+*/
 
 int main(int argc, char const *argv[]){
     struct listaCandidatos_t *candidatos; 
@@ -228,32 +327,28 @@ int main(int argc, char const *argv[]){
     candidatos   = criaLista();
     selecionados = criaLista();
 
-    printf("Lendo l: ");
     scanf("%d", &l);
-    printf("Lendo n: ");
     scanf("%d", &n); 
 
     for(int i = 0; i < n; ++i){
-        printf("Lendo maxgrupos: ");
         scanf("%d", &maxGrupos); 
         grupos = malloc((maxGrupos + 1)*sizeof(unsigned int));
         grupos[0] = maxGrupos;
         for(int k = 1; k < maxGrupos + 1; ++k){
-            printf("Lendo grupo: ");
             scanf("%d", &grupos[k]); 
         }
         nodo = criaNodo(i+1, grupos); 
         insereElementoLista(candidatos, nodo);
     }
-
     printLista(candidatos);
 
     melhorLista = copiaLista(candidatos);
 
-    printLista(melhorLista);
+    candidatos_branch_and_bound(candidatos, selecionados, l, 0);
 
-    if(temTodosOsGrupos(candidatos, l)==1)
-        printf("Esta certo, os candidatos tem todos os grupos\n");
+    printf("RESULTADO!!!!\n");
+    printLista(melhorLista);
+    printLista(selecionados);
 
     destroiLista(candidatos);
     destroiLista(selecionados);
